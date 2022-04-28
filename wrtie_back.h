5d0aad28c230a43bf51869c76c52a89c74304cd1
mijango <91990903+mijango@users.noreply.github.com>
2022-04-27 23:29:43 -0700

    adding variable rr and value to pass value

--- wrtie_back.h
+++ wrtie_back.h
@@ -1,21 +1,24 @@
 #include "write_back.h"
 #include <iostream>
 
-void write_back()
+void write_back(int rr, int value)
 {
+  //where rr represents the 70 and value represents 1b
+  //memory 0x70 is modified to 0x1b
   //writes back to register file
     if(reg_write = 0)
     {
-        d_mem[r] = value; 
+        d_mem[rr] = value; 
     }
-    if(mem_write = 0)
+    if(mem_write = 1)
     {
-        d_mem[r] = value;
+        d_mem[rr] = value;
     }
     else
     {
         reg_write = 1;
-        registe_file[r] = value;
+        registerfile[rr] = value;
+        registernames[rr];
     }
         
   //increment total_clocl_cycles by 1 when one instruction is finished
